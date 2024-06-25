9c74d03dfcde3cabafce48c6c1b7547a91e68a6f
gjohnn <juansgb316@outlook.com>
2024-06-25 19:19:19 -0300

    nashe

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const papu = {
+  name: "juan",
+  age: 19,
+  surname: "papu",
+};
+
+const papu2 = {
+  name: "nashe",
+  age: 20,
+  surname: ":v",
+};
+
+const arr = [];
+arr.push(papu);
+arr.push(papu2);
+
+arr.forEach((e) => {
+  const { name, age, surname } = e;
+  console.log(papunombre);
+  console.log(age);
+  console.log(surname);
+  console.log("-------------------");
+});

17c87df17f08ee284e25d29d9ad2fcfdca30941c
gjohnn <juansgb316@outlook.com>
2024-07-16 20:49:09 -0300

    Intentando images de nuevo

--- js/cakes.js
+++ js/cakes.js
@@ -3,19 +3,19 @@ const cakes = [
     name: "Cheesecake de Nutella",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "/imgs/prods/cakes/cheesecake_nutella.jpg",
+    image: "../imgs/prods/cakes/cheesecake_nutella.jpg",
   },
   {
     name: "Chocotorta",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "/imgs/prods/cakes/chocotorta.jpg",
+    image: "../imgs/prods/cakes/chocotorta.jpg",
   },
   {
     name: "Selva Negra",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "/imgs/prods/cakes/selva_negra.jpg",
+    image: "../imgs/prods/cakes/selva_negra.jpg",
   },
 ];
 

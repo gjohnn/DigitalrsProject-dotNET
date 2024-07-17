1878c783dad928316fa2b6ea6de77919e5b69d78
gjohnn <juansgb316@outlook.com>
2024-07-16 21:06:33 -0300

    intento images nro 39812083

--- js/cakes.js
+++ js/cakes.js
@@ -1,21 +1,24 @@
+const REPO_URL = "https://gjohnn.github.io/DigitalrsProject-dotNET/";
+const currentURL = window.location.origin + window.location.pathname.replace(/\/$/, "");
+const BASE_URL = currentURL.startsWith(REPO_URL) ? REPO_URL : "";
 const cakes = [
   {
     name: "Cheesecake de Nutella",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "../imgs/prods/cakes/cheesecake_nutella.jpg",
+    image: `${BASE_URL}/imgs/prods/cakes/cheesecake_nutella.jpg`,
   },
   {
     name: "Chocotorta",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "../imgs/prods/cakes/chocotorta.jpg",
+    image: `${BASE_URL}/imgs/prods/cakes/chocotorta.jpg`,
   },
   {
     name: "Selva Negra",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "../imgs/prods/cakes/selva_negra.jpg",
+    image: `${BASE_URL}/imgs/prods/cakes/selva_negra.jpg`,
   },
 ];
 
--- js/coffees.js
+++ js/coffees.js
@@ -1,22 +1,22 @@
-const coffees = [
+const REPO_URL = "https://gjohnn.github.io/DigitalrsProject-dotNET/";
+const currentURL = window.location.origin + window.location.pathname.replace(/\/$/, "");
+const BASE_URL = currentURL.startsWith(REPO_URL) ? REPO_URL : "";
+const cakes = [
   {
-    name: "Café Latte",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "../imgs/prods/coffees/coffee_latte.jpg",
+    name: "Cheesecake de Nutella",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
+    image: `${BASE_URL}/imgs/prods/cakes/cheesecake_nutella.jpg`,
   },
   {
-    name: "Café Americano",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "../imgs/prods/coffees/coffee_americano.jpg",
+    name: "Chocotorta",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
+    image: `${BASE_URL}/imgs/prods/cakes/chocotorta.jpg`,
   },
   {
-    name: "Cappuccino",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
-    image: "../imgs/prods/coffees/capuccino.jpg",
+    name: "Selva Negra",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sit amet justo nec ligula blandit scelerisque.",
+    image: `${BASE_URL}/imgs/prods/cakes/selva_negra.jpg`,
   },
 ];
 
-export default coffees;
+export default cakes;
